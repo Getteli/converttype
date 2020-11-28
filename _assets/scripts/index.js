7d206128ba3336b18d5d9ee493eb827414d04fa7
getteli <Douglas_araujo018@outlook.com>
2020-11-28 19:18:20 -0300

    ajuste no index.js

--- _assets/scripts/index.js
+++ _assets/scripts/index.js
@@ -4,9 +4,3 @@ const InstanceDropdownMenuMobile = M.Sidenav.init(ElemensDropdownMenuMobile,{
     edge: "right",
     draggable: true,
 });
-var $input    = document.getElementById('input-file2'),
-    $fileName = document.getElementById('file-name2');
-$input.addEventListener('change', function(){
-  var namefile = this.value.split("\\")[2];
-  $fileName.textContent = namefile;
-});
